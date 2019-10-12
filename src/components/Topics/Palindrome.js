9a1ea103a1606d2b294fc8ec6d6b37355e1771c5
WillHoll <willhollingsworth15@yahoo.com>
2019-10-11 20:18:52 -0600

    good ending point

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class Palindrome extends Component{
+    constructor() {
+        super();
+
+    }
     render() {
         return(
-            <p>Palindrome component</p>
+            <div className="puzzleBox filterStringPB">
+                <h4> Filter String </h4>
+                <span className="puzzleText"> Names:</span>
+                <input className="inputLine"></input>
+                <button className="confirmationButton" > Filter </button>
+                <span className="resultsBox filterStringRB"> Filtered Names: </span>
+            </div>
         )
     }
 }
+
 
 export default Palindrome;
